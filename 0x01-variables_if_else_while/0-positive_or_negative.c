dca659646f58d8c85c89968f358c71dffdbcfdcd
Reza Khakshouri <1885@holbertonschool.com>
2020-06-14 16:47:39 -0400

    initial commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,32 @@
-hello
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+* main - time determined variable
+* description: time
+* return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+	if  (n == 0)
+{
+printf("%d is 0\n", n);
+}
+	if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+
+	return (0);
+}
