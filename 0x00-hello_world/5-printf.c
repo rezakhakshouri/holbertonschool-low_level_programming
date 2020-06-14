7f53039d6620fd26b2ba1624c62782f3f021dce4
Reza Khakshouri <1885@holbertonschool.com>
2020-06-14 09:21:38 -0400

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main(void)
+/*
+* main 0 through 9
+* description: simple numerical value
+* Return: 0
+*/
+
 {
 
 	printf("with proper grammar, but the outcome is a piece of art, \n");
