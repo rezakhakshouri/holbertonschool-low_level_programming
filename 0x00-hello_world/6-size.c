c884f7014853796bfed90b43272b3bf84ec4188c
Reza Khakshouri <1885@holbertonschool.com>
2020-06-15 11:34:58 -0400

    initial commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - 0 through 9
+* description: counts characters
+* Return: 0
+*/
+
+int main(void)
+{
+printf("Size of a char: %d", sizeof(char));
+printf(" byte(s)\n");
+printf("Size of an int: %d", sizeof(int));
+printf(" byte(s)\n");
+printf("Size of a long int: %d", sizeof(long int));
+printf(" byte(s)\n");
+printf("Size of a long long int: %d", sizeof(long long int));
+printf(" byte(s)\n");
+printf("Size of a float: %d", sizeof(float));
+printf(" byte(s)\n");
+
+}
+
