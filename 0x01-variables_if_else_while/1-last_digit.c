519c14d76b78d59f80f2cdc301f466ca745965ab
Reza Khakshouri <1885@holbertonschool.com>
2020-06-15 16:59:51 -0400

    betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,18 +17,23 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	c = (n % 10);
 	printf("last digit of: %d is %d", n, c);
-if (c == 0)
-{
-printf(" and is 0\n");
-}
-else if (c > 5)
-{
-printf(" and is greater than 5\n");
-}
-else(c < 6 && c != 0);
-{
-printf(" and is less than 6 and not 0\n");
-}
-
-return (0);
+	if (c == 0)
+	{
+		printf(" and is 0\n");
+	}
+	else
+	{
+		if (c > 5)
+		{
+			printf(" and is greater than 5\n");
+		}
+		else
+		{
+			if (c < 6 && c != 0)
+			{
+				printf(" and is less than 6 and not 0\n");
+			}
+		}
+	}
+	return (0);
 }
