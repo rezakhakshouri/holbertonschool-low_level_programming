56d430dc552d1afef6168d6a342415356b8a94ca
Reza Khakshouri <1885@holbertonschool.com>
2020-06-16 00:16:30 -0400

    without line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - 0 through 9
+* description: gets numbers
+* Return: 0
+*/
+
+int main(void)
+{
+	int m;
+
+	m = 97;
+	while (m <= 122)
+	{
+		putchar(m);
+		m = m + 1;
+	}
+
+return (0);
+}
