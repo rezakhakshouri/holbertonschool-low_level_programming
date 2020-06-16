d556fba212d1280d80b0c3e5ba834d0dde978128
Reza Khakshouri <1885@holbertonschool.com>
2020-06-16 06:07:07 -0400

    betty

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - 0 through 9
+* description: gets numbers
+* Return: 0
+*/
+
+int main(void)
+{
+	char c;
+	char b;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+{
+	putchar(c);
+}
+	b == 'A';
+	while (b <= 'Z')
+{	putchar(b);
+}
+{
+	putchar('\n');
+}
+	return (0);
+}
