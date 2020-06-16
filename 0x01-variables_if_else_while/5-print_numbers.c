83050fcb28f155a5ce938cadf7ca128492b60a97
Reza Khakshouri <1885@holbertonschool.com>
2020-06-16 06:27:37 -0400

    initial commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - 0 through 9
+* description: gets numbers
+* Return: 0
+*/
+
+int main(void)
+{
+	int m;
+
+	m = 48;
+	while (m <= 57)
+	{
+		putchar(m);
+		m = m + 1;
+	}
+	{
+		putchar('\n');
+	}
+return (0);
+}
