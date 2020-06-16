94bdf1e94488d4fbc8cda34f534fadd956f90435
Reza Khakshouri <1885@holbertonschool.com>
2020-06-16 06:38:57 -0400

    initial commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - 0 through 9
+* description: gets numbers
+* Return: 0
+*/
+
+int main(void)
+{
+	int m;
+
+	m = 122;
+	while (m >= 97)
+	{
+		putchar(m);
+		m = m - 1;
+	}
+	{
+		putchar('\n');
+	}
+return (0);
+}
