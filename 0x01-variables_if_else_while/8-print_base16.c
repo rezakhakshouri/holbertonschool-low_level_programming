1e1bb1a213886edbb3ceea6707c85f8c96b8fd6a
Reza Khakshouri <1885@holbertonschool.com>
2020-06-16 07:14:30 -0400

    initial commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - 0 through 9
+* description: gets numbers
+* Return: 0
+*/
+
+int main(void)
+{
+	int m;
+	char k;
+
+	k = '1';
+	while (k <= '9')
+	{
+	putchar(k);
+	k = k + 1;
+	m = 97;
+	}
+	while (m <= 102)
+	{
+		putchar(m);
+		m = m + 1;
+	}
+	{
+		putchar('\n');
+	}
+return (0);
+}
