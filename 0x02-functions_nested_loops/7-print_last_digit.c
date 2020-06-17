f7ac3cd7efc6a9f3f905ad49ca020d0d497357df
Reza Khakshouri <1885@holbertonschool.com>
2020-06-17 16:12:49 -0400

    betty

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,13 @@
 #include "holberton.h"
-
+/**
+ * print_last_digit - prints last digit of
+ * @n: takes the input
+ * Return: returns last digit of the numbers
+ */
 int print_last_digit(int n)
 {
 	n = n % 10;
-	if(n < 0)
+	if (n < 0)
 	{
 		n = -n;
 	}
