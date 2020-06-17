ee5aa90b48d3c1295f1ff68f63c12a04ca496091
Reza Khakshouri <1885@holbertonschool.com>
2020-06-17 01:58:22 -0400

    initial commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* _islower - selects lower case
+* description : checks for lowercase
+* Return: 1 if c is lowercase and 0 otherwise
+*/
+
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
