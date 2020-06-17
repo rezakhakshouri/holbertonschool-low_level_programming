8911bbd3a0546eabf81192c26a5964a606d91f6a
Reza Khakshouri <1885@holbertonschool.com>
2020-06-17 17:28:11 -0400

    initial commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * jack_bauer - calender
+ * Return: always hours
+ */
+
+void jack_bauer(void)
+{
+
+	int mins, hours;
+
+	for (hours = 0 ; mins <= 23; hours++)
+	{
+		for  (mins = 0 ; mins <= 59 ; mins++)
+		{
+			_putchar('0' + hours / 10);
+			_putchar('0' + hours % 10);
+			_putchar(' ');
+			_putchar('0' + mins / 10);
+			_putchar('0' + mins % 10);
+			_putchar('\n');
+		}
+
+	}
+
+
+
+
+
+}
