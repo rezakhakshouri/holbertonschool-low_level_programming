e85d601a04706b8a8546bfb6e9dfb02ea973f356
Reza Khakshouri <1885@holbertonschool.com>
2020-06-16 23:35:40 -0400

    initial commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+* print_alphabet - prints alphabet
+* description: alphabet in lower case
+* Return: 0
+*/
+
+void print_alphabet(void)
+
+{
+	int c;
+	int b;
+
+	b = 1;
+	while (b <= 10)
+	{
+		c = 97;
+		while (c <= 122)
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		b++;
+	}
+}
