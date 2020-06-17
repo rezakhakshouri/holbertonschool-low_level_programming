2b56e459ee653f1cc1148878d6df3dfba9529287
Reza Khakshouri <1885@holbertonschool.com>
2020-06-16 22:50:32 -0400

    initial commit

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
 *main - 0 through 9
 *description: printing
@@ -7,6 +7,16 @@
 
 int main(void)
 {
-	printf("Holberton\n");
-	return (0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return(0);
 }
