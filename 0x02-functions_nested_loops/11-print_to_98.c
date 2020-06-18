a1337e349c70dbb64fab6edfa1e52e8b75be9d92
Reza Khakshouri <1885@holbertonschool.com>
2020-06-18 05:53:56 -0400

    initila commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints to 98
+ * @n: a number
+ * Return: 98
+ */
+void print_to_98(int n)
+{
+	if(n > 98)
+	{
+		n = n;
+		while(n >= 98)
+		{
+			printf("%d ",n);
+			n--;
+		}
+		printf("\n");
+	}
+	else if(n < 98)
+	{
+		n  = n;
+		while(n <= 98)
+		{
+			printf("%d ",n);
+			n++;
+		}
+		printf("\n");
+	}
+	else
+	printf("98\n");
+
+}
