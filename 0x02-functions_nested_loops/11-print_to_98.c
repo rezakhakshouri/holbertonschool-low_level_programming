9f1d01212693ad7c28231bc8c3fa038d4c13615c
Reza Khakshouri <1885@holbertonschool.com>
2020-06-18 05:59:52 -0400

    initila commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,22 +7,22 @@
  */
 void print_to_98(int n)
 {
-	if(n > 98)
+	if (n > 98)
 	{
 		n = n;
-		while(n >= 98)
+		while (n >= 98)
 		{
-			printf("%d ",n);
+			printf("%d ", n);
 			n--;
 		}
-		printf("\n");
+		print ("\n");
 	}
-	else if(n < 98)
+	else if (n < 98)
 	{
 		n  = n;
-		while(n <= 98)
+		while (n <= 98)
 		{
-			printf("%d ",n);
+			print ("%d ", n);
 			n++;
 		}
 		printf("\n");
