e8751cc92670319f67a0411cab5d1720517a6d17
Reza Khakshouri <1885@holbertonschool.com>
2020-06-18 10:37:18 -0400

    ,

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,14 @@ void print_to_98(int n)
 		n = n;
 		while (n >= 98)
 		{
-			printf("%d ", n);
+			if (n > 98)
+			{
+				printf("%d ,", n);
+			}
+			else
+			{
+				printf("%d", n);
+			}
 			n--;
 		}
 		printf("\n");
@@ -22,7 +29,14 @@ void print_to_98(int n)
 		n  = n;
 		while (n <= 98)
 		{
-			printf("%d ", n);
+			if (n < 98)
+			{
+				printf("%d ,", n);
+			}
+			else
+			{
+				printf("%d", n);
+			}
 			n++;
 		}
 		printf("\n");
